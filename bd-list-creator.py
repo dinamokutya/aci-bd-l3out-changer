7b96d6a93b023427d0b4b57d5e096553c8be1d89
dinamokutya <dinamokutya@gmail.com>
2023-10-10 20:35:21 +0200

    first

---
+++ bd-list-creator.py
@@ -0,0 +1,18 @@
+import json
+
+VRF = 'VRF-ANDRISKA-IAC'
+
+f = open('fvBD.json')
+f2 = open("bd-list.txt", "w")
+data = json.load(f)
+length = len(data)
+
+#print (data[0]['fvBD']['children'][3]['fvRsCtx']['attributes']['tnFvCtxName'])
+
+for i in range(length):
+    print (data[i]['fvBD']['attributes']['name'] + "," + data[i]['fvBD']['children'][3]['fvRsCtx']['attributes']['tnFvCtxName'])
+    if data[i]['fvBD']['children'][3]['fvRsCtx']['attributes']['tnFvCtxName'] == VRF :
+        f2.write (data[i]['fvBD']['attributes']['name'] + "," + data[i]['fvBD']['children'][3]['fvRsCtx']['attributes']['tnFvCtxName'] )
+        f2.write("\n")
+f.close()
+f2.close()
